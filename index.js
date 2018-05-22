253820d2bdf7b86af91fab9bb6c3703a3d14ccda
Haowei Zhang <hwzhang@fortinet.com>
2018-05-22 10:57:48 -0700

    import project (TODO: read me)
    Change-Id: Iae71527f16bcf55f5f9dc1325ff646d4d78ae67c

---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/*
+Author: Fortinet
+
+The following Lambda function will be used for FortiOS log storage.
+
+Currently the script has the following configurations (By environment variable):
+
+TABLE_NAME: (DynamoDB table name)
+
+*/
+
+
+console.log('Loading Log Storage lambda function');
+
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+function FStitchLog(params) {
+    // primary fields
+    this.serial = params.data.sn;
+    this.logId = params.data.rawlog.logid;
+
+    // information fields
+    this.email = params.email || '';
+    this.stitchName = params.data.stitch;
+    this.eventType = params.data.eventtype,
+    this.timestamp = params.data.time * 1000;
+
+    // log fields
+    this.log = params.data.rawlog;
+}
+
+exports.handler = (event, context, callback) => {
+    console.log('Request received: \n' + JSON.stringify(event));
+    console.log('Context received: \n' + JSON.stringify(context));
+
+    let item;
+
+    try {
+        item = new FStitchLog(event);
+    } catch (e) {
+        callback({
+            errorMessage: 'Missing necessary log field.'
+        });
+        return;
+    }
+
+    console.log('Item init: ' + JSON.stringify(item, null, 2));
+
+    dynamodb.put({
+        TableName: process.env.TABLE_NAME,
+        Item: item
+    }, function(error, data) {
+        if (error) {
+            let errorMessage = JSON.stringify(error, null, 2);
+            console.log('Failed to get for: ' + errorMessage);
+            callback(errorMessage);
+        } else {
+            console.log('DynamoDB put succeeded: ' + JSON.stringify(data, null, 2));
+            callback(null, item);
+        }
+    });
+};
